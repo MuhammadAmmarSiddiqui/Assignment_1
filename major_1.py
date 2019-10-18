c7d89088fc4a1640a89931af3b6a8972adc64288
Muhammad Ammar Siddiqui <ammar-sidd@hotmail.com>
2019-10-18 19:00:53 +0500

    Converted 20 images into a numpy array. All errors fixed

--- major_1.py
+++ major_1.py
@@ -3,16 +3,15 @@ import numpy as np
 from PIL import Image
 files = os.listdir(".")
 im_list = []
-im = []
-#im = np.zeros([20, 500, 500, 3])
+np_photos = np.zeros([20, 200, 200, 3])
 for name in files:
     if name.endswith(".jpg"):
         im_list.append(name)
 for i in range(len(im_list)):
-    a = np.array(Image.open(im_list[i]))
-    im.append(a)
-    
-print(type(im))
-images = np.array(im)
-print(type(images))
-print(images.shape)
+    a = Image.open(im_list[i])
+    rs = a.resize((200,200))
+    cv_pic = np.array(rs)
+    np_photos[i] = cv_pic
+print(type(np_photos))
+print(np_photos.shape)
+print(np_photos[:20, :10, :10, :3])
