14cf8a26cb41c427312230686f45fdb2efd59aa1
Muhammad Ammar Siddiqui <ammar-sidd@hotmail.com>
2019-10-26 15:41:05 +0500

    Added functions in the program

--- major_1.py
+++ major_1.py
@@ -1,17 +1,37 @@
 import os
 import numpy as np
 from PIL import Image
-files = os.listdir(".")
 im_list = []
-np_photos = np.zeros([20, 200, 200, 3])
-for name in files:
-    if name.endswith(".jpg"):
-        im_list.append(name)
-for i in range(len(im_list)):
-    a = Image.open(im_list[i])
-    rs = a.resize((200,200))
-    cv_pic = np.array(rs)
-    np_photos[i] = cv_pic
-print(type(np_photos))
-print(np_photos.shape)
-print(np_photos[:20, :10, :10, :3])
+def trav():
+    try:
+        files = os.listdir(".")
+        for name in files: 
+            if name.endswith(".jpg"):
+                im_list.append(name)
+    except: 
+        print('No Image found in directory')
+    
+def num_arr_len():
+    np_photos = np.zeros([len(im_list), 200, 200, 3])
+    return np_photos
+
+def image_numarray_resize():
+    try:
+        trav()
+        np_photos = num_arr_len()
+        for i in range(len(im_list)):
+            image = Image.open(im_list[i])
+            resize = image.resize((200,200))
+            np_photo = np.array(resize)
+            np_photos[i] = np_photo
+        return np_photos
+    except:
+        print('Unable to convert images into numpy array')
+
+def main():
+    numpy_array = image_numarray_resize()
+    print(type(numpy_array))
+    print('The shape of numpy_array is :', numpy_array.shape)
+
+if __name__ ==  "__main__":
+    main()
